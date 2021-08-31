400cde1560d4d0311b1f705998fa887032983c38
Gryfikki <philippe_gense@hotmail.fr>
2021-08-31 09:28:22 +0200

    Commit back services métiers fix

--- src/main/java/com/inti/controllers/UtilisateurController.java
+++ src/main/java/com/inti/controllers/UtilisateurController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.entities.Annonce;
@@ -50,6 +51,8 @@ public class UtilisateurController {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 
+	
+	//CRUD
 	@PostMapping("/users")
 	public Utilisateur saveUtilisateur(@RequestBody Utilisateur utilisateur) {
 		Utilisateur user = new Utilisateur();
@@ -76,9 +79,11 @@ public class UtilisateurController {
 		service.deleteUtilisateur(id);
 	}
 
+	
+	//Services métiers
 	@PostMapping("/course")
-	public Course creerCourse(@RequestBody String arrivee, @RequestBody String depart, @RequestBody Date horaire,
-			@RequestBody Utilisateur chauffeur) {
+	public Course creerCourse(@RequestParam("arrivee") String arrivee, @RequestParam("depart") String depart, @RequestParam("horaire") Date horaire,
+			@RequestParam("chauffeur") Utilisateur chauffeur) {
 
 		Random rand = new Random();
 				
@@ -93,7 +98,7 @@ public class UtilisateurController {
 	}
 
 	@PostMapping("/reservation")
-	public Reservation creerReservation(@RequestBody Course course, @RequestBody List<Utilisateur> clients) {
+	public Reservation creerReservation(@RequestParam("course") Course course, @RequestParam("clients") List<Utilisateur> clients) {
 
 		Reservation resa = new Reservation();
 		resa.setCourse(course);
@@ -103,7 +108,7 @@ public class UtilisateurController {
 	}
 
 	@PostMapping("/evaluation")
-	public Evaluation creerEval(@RequestBody String com,@RequestBody Utilisateur client,@RequestBody Utilisateur chauffeur,@RequestBody double note) {
+	public Evaluation creerEval(@RequestParam("commentaire") String com,@RequestParam("client") Utilisateur client,@RequestParam("chauffeur") Utilisateur chauffeur,@RequestParam("note") double note) {
 		
 		Evaluation eval = new Evaluation(chauffeur, note);
 		eval.setCommentaire(com);
@@ -113,7 +118,7 @@ public class UtilisateurController {
 	}
 	
 	@PostMapping("/commentaire")
-	public Commentaire creerCom(@RequestBody String com,@RequestBody Utilisateur client,@RequestBody Annonce annonce) {
+	public Commentaire creerCom(@RequestParam("commentaire") String com,@RequestParam("client") Utilisateur client,@RequestParam("annonce") Annonce annonce) {
 		
 		Commentaire commentaire = new Commentaire(annonce);
 		commentaire.setClient(client);
@@ -123,7 +128,7 @@ public class UtilisateurController {
 	}
 	
 	@PostMapping("/reclamation")
-	public Reclamation creerReclam(@RequestBody String com,@RequestBody Utilisateur client,@RequestBody Course course) {
+	public Reclamation creerReclam(@RequestParam("commentaire") String com,@RequestParam("client") Utilisateur client,@RequestParam("course") Course course) {
 		
 		Reclamation reclam = new Reclamation(false,course);
 		reclam.setClient(client);
